8c0f162c49503e2aa501a4e87566a39888b56e47
Stephan_Zeibig <stephanzeibig@gmx.de>
2025-05-03 15:46:38 +0200

    k: bugfixes

--- src/build.sh
+++ src/build.sh
@@ -1,1 +1,8 @@
-python3 src/main.py "static_site_generator"
+
+REPO_NAME="/static_site_generator"
+
+# 1. Assets kopieren
+cp -r static/* docs/
+
+# 2. Seiten generieren
+python3 src/main.py "$REPO_NAME"
--- src/main.py
+++ src/main.py
@@ -1,20 +1,19 @@
 from textnode import *
 import sys
 
-#base_path = sys.argv[0]
-
 
 def main():
+
+    repo_base = sys.argv[1] if len(sys.argv) > 1 else ""
+    
     source_folder      = "static"
     destination_folder = "docs"
     content_path       = "content/"
     template_path      = "templates.html"
 
-    # DER WICHTIGE TEIL:
-    # setze basepath auf den GitHub-Pages-Pfad:
-    repo_base = "/static_site_generator"
-
     copy_all_content(source_folder, destination_folder)
+
+    # gib repo_base an die rekursive Funktion weiter!
     generate_pages_recursive(content_path, template_path, destination_folder, repo_base)
     
 if __name__ == "__main__":
